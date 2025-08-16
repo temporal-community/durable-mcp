2660fbc8066c7f2a57d40fcb2be736549ae06c6e
Cornelia Davis <cornelia.davis@temporal.io>
2025-08-16 13:26:59 -0700

    first crack at retrieving URL contents
    Signed-off-by: Cornelia Davis <cornelia.davis@temporal.io>

--- workflows/hackernews_worker.py
+++ workflows/hackernews_worker.py
@@ -5,7 +5,7 @@ from temporalio.client import Client
 from temporalio.worker import Worker
 
 from workflows.workflows import GetLatestStories
-from workflows.activities import make_hackernews_request
+from workflows.activities import make_hackernews_request, fetch_url_content, render_url_content
 
 async def main():
     client = await Client.connect("localhost:7233")
@@ -14,7 +14,7 @@ async def main():
         client,
         task_queue="hackernews-task-queue",
         workflows=[GetLatestStories],
-        activities=[make_hackernews_request],
+        activities=[make_hackernews_request, fetch_url_content, render_url_content],
     )
     print("Hacker News worker started. Listening for workflows...")
     await worker.run()
